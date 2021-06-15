3b7dd81d6b774c7bc116789275476a4c6145b220
pranav21999 <84624284+pranav21999@users.noreply.github.com>
2021-06-16 01:50:03 +0530

    calculate length of two lines

--- linecomparison.java
+++ linecomparison.java
@@ -1,7 +1,40 @@
+import java.util.*;
 class linecomparison
 {
+	//variables
+	int x1,y1,x2,y2;
+	int ans=0;
+	void welcome()//method to print welcome msg.
+	{
+		System.out.println("Welcome to empwage computation");
+	}
+		
+	 void calculate() // method to calulate length of line using cartetion system
+	{
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter x1, for line : ");
+		int x1 = sc.nextInt();
+		System.out.print("Enter y1, for line1 : ");
+		int y1 = sc.nextInt();
+		System.out.print("Enter x2, for line1 : ");
+		int x2 = sc.nextInt();
+		System.out.print("Enter y2, for line1 : ");
+		int y2 = sc.nextInt();
+		double ans = Math.sqrt(Math.pow( (x2-x1),2 ) + Math.pow( (y2-y1),2));
+		System.out.println("length of line is:"+ans);
+	}
 	public static void main(String args[])
 	{
-		System.out.println("Welcome to line comparison problem");
+		linecomparison line1=new linecomparison();
+		line1.welcome();		
+		System.out.println("coordinates of line 1 are");
+		line1.calculate();
+		
+		System.out.println("coordinates of line 2 are");
+		linecomparison line2=new linecomparison();
+		line2.calculate();
+		
+		
 	}
+	
 }
